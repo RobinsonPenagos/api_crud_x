4dc7903b97ecbafd0f3641ae92fb9f92613073ec
RobinsonPenagos <103594345+RobinsonPenagos@users.noreply.github.com>
2023-04-21 00:09:16 -0500

    update

--- src/database.js
+++ src/database.js
@@ -1,20 +1,17 @@
-
-
-import { Sequelize } from 'sequelize';
-import {DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT} from './config'
+import { Sequelize } from "sequelize";
+import { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT } from "./config";
 
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-
   host: DB_HOST,
-  dialect: 'mysql',
-  port: DB_PORT
+  dialect: "mysql",
+  port: DB_PORT,
 });
 
 try {
   sequelize.authenticate();
-  console.log('Conexion exitosa a mysql.');
+  console.log("Conexion exitosa a mysql.");
 } catch (error) {
-  console.error('No se establecio la conexion a la db:', error);
+  console.error("No se establecio la conexion a la db:", error);
 }
 
 export default sequelize;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,4 @@ import app from './app';
 import './database'
 import { PORT } from './config';
 
-require('dotenv').config()
-
-const port = PORT;
-
-app.listen(port, console.log('conectado al puerto ', port))
+app.listen(PORT, console.log('conectado al puerto ', PORT))
